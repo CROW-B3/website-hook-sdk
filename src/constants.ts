1d44b699e83021059636501b62a48891f1096a94
Methika Fernando <methikafernando2006@gmail.com>
2026-01-29 19:45:44 +0530

    refactor: update base URL to localhost:8013

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 import { getEnvironmentVariable } from './utils/environment';
 
 export const NEXT_BASE_URL =
-  getEnvironmentVariable('NEXT_PUBLIC_BE_BASE_URL') || 'http://localhost:3001';
+  getEnvironmentVariable('NEXT_PUBLIC_BE_BASE_URL') || 'http://localhost:8013';
