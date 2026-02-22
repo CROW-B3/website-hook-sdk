755aa3bda7ee6107ff722917c1678aca86ccd928
Methika Fernando <methikafernando2006@gmail.com>
2026-02-22 17:53:48 +0530

    fix: add https:// prefix to dev ingest worker base URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 import { getEnvironmentVariable } from './utils/environment';
 
 export const NEXT_BASE_URL =
-  getEnvironmentVariable('NEXT_PUBLIC_BE_BASE_URL') || 'dev.internal.ingest-worker.crowai.dev';
+  getEnvironmentVariable('NEXT_PUBLIC_BE_BASE_URL') || 'https://dev.internal.ingest-worker.crowai.dev';
