53855df54e978a2d50712d63e416b03eac74a787
Methika Fernando <methikafernando2006@gmail.com>
2026-03-20 02:03:36 +0530

    fix: hardcode base URL and remove environment variable dependency

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
 import { getEnvironmentVariable } from './utils/environment';
 
-export const NEXT_BASE_URL =
-  getEnvironmentVariable('NEXT_PUBLIC_BE_BASE_URL') ||
-  'https://dev.internal.ingest-worker.crowai.dev';
+export const NEXT_BASE_URL = 'https://dev.internal.ingest-worker.crowai.dev';
